a6ca776389da18b2ad0433e9a541655143183636
skyronnn <81257931+skyronnn@users.noreply.github.com>
2021-05-04 22:35:44 +0200

    spaceinvaders main

---
+++ SpaceInvaders.java
@@ -0,0 +1,66 @@
+ package fr.unilim.iut.spaceinvaders;
+  
+ public class SpaceInvaders {
+
+	 private static final char MARQUE_FIN_LIGNE = '\n';
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_VAISSEAU = 'V';
+	int longueur;
+	 int hauteur;
+	 Vaisseau vaisseau;
+
+	 public SpaceInvaders(int longueur, int hauteur) {
+		   this.longueur = longueur;
+		   this.hauteur = hauteur;
+	}
+	 
+	 @Override
+		public String toString() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
+				}
+				espaceDeJeu.append(MARQUE_FIN_LIGNE);
+			}
+			return espaceDeJeu.toString();
+		}
+
+	private char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque=MARQUE_VAISSEAU;
+		else
+		      marque=MARQUE_VIDE;
+		return marque;
+	}
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
+	}
+
+	 public void positionnerUnNouveauVaisseau(int x, int y) {
+	        this.vaisseau = new Vaisseau(x, y);
+
+		}
+	 public class Vaisseau {
+
+			int x;
+			int y;
+
+			public Vaisseau(int x, int y) {
+				this.x = x;
+				this.y = y;
+			}
+
+			public boolean occupeLaPosition(int x, int y) {
+				return (this.x==x) && (this.y==y);
+			}
+
+		}
+
+}
