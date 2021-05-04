05c088ee410ff8b10836ba5f24a28a309579cd47
skyronnn <81257931+skyronnn@users.noreply.github.com>
2021-05-04 22:36:11 +0200

    Add files via upload

---
+++ SpaceInvadersTest.java
@@ -0,0 +1,42 @@
+package fr.unilim.iut.spaceinvaders;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SpaceInvadersTest {
+	
+	@Test
+	public void test_AuDebut_JeuSpaceInvaderEstVide() {
+		    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		    assertEquals("" + 
+		    "...............\n" + 
+		    "...............\n" +
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" , spaceinvaders.toString());
+	     }
+
+
+@Test
+public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
+	SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+	spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+	assertEquals("" + 
+	"...............\n" + 
+	"...............\n" +
+	"...............\n" + 
+	"...............\n" + 
+	"...............\n" + 
+	"...............\n" + 
+	"...............\n" + 
+	"...............\n" + 
+	"...............\n" + 
+	".......V.......\n" , spaceinvaders.toString());
+}
+
+}
